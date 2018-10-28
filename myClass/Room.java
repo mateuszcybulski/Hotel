9c6ef36736a974eb1f5fc529d39d11fab25af89b
mateuszcybulski <mateuszcybulski345@gmail.com>
2018-10-28 12:38:37 +0100

    add Room class

---
+++ myClass/Room.java
@@ -0,0 +1,34 @@
+package myClass;
+
+public class Room {
+	
+	int numer, id, floor;
+	String description;
+	
+	
+	public int getNumer() {
+		return numer;
+	}
+	public void setNumer(int numer) {
+		this.numer = numer;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getFloor() {
+		return floor;
+	}
+	public void setFloor(int floor) {
+		this.floor = floor;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
